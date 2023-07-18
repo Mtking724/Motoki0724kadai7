d3a5e15511c87724dc8990df68cdc31015b288a8
akio0911 <akio0911@gmail.com>
2023-07-18 19:30:48 +0900

    feedback

--- Motoki0724kadai7/FirstViewController.swift
+++ Motoki0724kadai7/FirstViewController.swift
@@ -1,5 +1,5 @@
 //
-//  secondViewController.swift
+//  FirstViewController.swift
 //  Motoki0724kadai7
 //
 //  Created by Motoki Okayasu on 2023/07/10.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class secondViewController: UIViewController {
+class FirstViewController: UIViewController {
     
     //textField,labelをoutlet接続
     @IBOutlet private weak var textField1: UITextField!
@@ -19,34 +19,21 @@ class secondViewController: UIViewController {
         super.viewDidLoad()
         textField1.keyboardType = .asciiCapableNumberPad
         textField2.keyboardType = .asciiCapableNumberPad
-        // Do any additional setup after loading the view.
     }
     
-    //メソッドで引き算の仕様を定義する
-    private func subtraction() {
+    //メソッドで足し算の仕様を定義する
+    private func addition() {
         let value1 = Int(textField1.text ?? "") ?? 0
         let value2 = Int(textField2.text ?? "") ?? 0
         
-        resultLabel.text = String(value1 - value2)
+        resultLabel.text = String(value1 + value2)
     }
     
     //ButtonをAction接続する
-    @IBAction private func tapButton(_ sender: Any) {
+    @IBAction private func tapButtom(_ sender: Any) {
         //resignFirstResponderメソッドでボタンを押すたびにキーボードを隠す
         self.textField1.resignFirstResponder()
         self.textField2.resignFirstResponder()
-        subtraction()
+        addition()
     }
-    
-    
-    /*
-     // MARK: - Navigation
-     
-     // In a storyboard-based application, you will often want to do a little preparation before navigation
-     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-     // Get the new view controller using segue.destination.
-     // Pass the selected object to the new view controller.
-     }
-     */
-    
 }
